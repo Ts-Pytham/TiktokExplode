362fa1c44e5993ea350cb6b444de88367b21b478
Johan Sánchez <johansanchezal@unimagdalena.edu.co>
2026-06-12 10:46:11 -0500

    HttpFetcher has been improved to avoid WAF detection. Integration tests for HttpFetcher and TiktokClient.

--- TiktokExplode.IntegrationTests/Usings.cs
+++ TiktokExplode.IntegrationTests/Usings.cs
@@ -4,3 +4,6 @@ global using System.Text.Json.Nodes;
 global using TiktokExplode.Infrastructure.Fetchers.Search;
 global using TiktokExplode.IntegrationTests.Fixtures;
 global using TiktokExplode.Infrastructure.Clients;
+global using TiktokExplode.Infrastructure.Fetchers;
+global using TiktokExplode.Domain.Exceptions;
+global using TiktokExplode.Infrastructure.Options;
---
+++ TiktokExplode.IntegrationTests/Videos/TiktokClientTests.cs
@@ -0,0 +1,27 @@
+﻿namespace TiktokExplode.IntegrationTests.Videos;
+
+public sealed class TiktokClientTests
+{
+    [Fact]
+    public async Task GetVideoAsync_Should_Return_Video()
+    {
+        var options = new TiktokOptions
+        {
+            MaxWafRetries = 10,
+            RetryBaseDelay = TimeSpan.FromMilliseconds(200),
+        };
+
+        await using var client = TiktokClient.CreateWithHttp(options: options);
+
+        var video = await client.GetVideoAsync(
+            "https://www.tiktok.com/@js_nightwave/video/7579504710961548565");
+
+        video.Should().NotBeNull();
+
+        video.Id.Should().NotBeNullOrWhiteSpace();
+
+        video.Info.DownloadLinks.Should().NotBeNull();
+
+        video.Author.Should().NotBeNull();
+    }
+}
